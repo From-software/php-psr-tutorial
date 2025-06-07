8aa268f4fe49c7cce916eebc665a9c2cdb8c4e37
Kevin From <kevin@kevinfrom.dk>
2025-06-07 12:13:29 +0200

    created `dump` and `dd` debug functions

--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
 <?php
 declare(strict_types=1);
 
-phpinfo();
+function dump(mixed $data): void
+{
+    echo '<pre style="background: black; color: green; padding: 3px 5px;">' . print_r($data, true) . '</pre>';
+}
+
+function dd(mixed $data): never
+{
+    dump($data);
+    die();
+}
+
+dump('test');
+dd($_SERVER['REQUEST_URI']);
