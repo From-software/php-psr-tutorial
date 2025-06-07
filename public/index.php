2d999273fbb9e39cac67854db001e021d61a2079
Kevin From <kevin@kevinfrom.dk>
2025-06-07 12:57:10 +0200

    added comment

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Front controller for the application.
+ * Any request which does not match a file in the `public` directory, will be routed to this file.
+ */
+
 function dump(mixed $data): void
 {
     echo '<pre style="background: black; color: green; padding: 3px 5px;">' . print_r($data, true) . '</pre>';
